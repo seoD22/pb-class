92e7cca8703b8a451fbba0517d3341f4b65d2aad
seoD22 <cobbt258@naver.com>
2023-03-29 15:03:27 +0900

    scroll event 적용

--- week2-basic/js/week2.js
+++ week2-basic/js/week2.js
@@ -1,14 +1,21 @@
-window.addEventListener("scroll", function(){
-  var sections = document.querySelectorAll(".reveal")
-  for (var i = 0; i < sections.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = sections[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-      
-      if (elementTop < windowHeight - elementVisible) {
-        sections[i].classList.add("active");
-      } else {
-        sections[i].classList.remove("active");
+window.onload = function(){
+  let observer = new IntersectionObserver((e)=>{
+    e.forEach((it)=>{
+      if(it.isIntersecting){
+        it.target.style.opacity = 1
+        it.target.style. transform = 'translateY(0)'
       }
-  }
-});
+      // else{
+      //   it.target.style.opacity = 0
+      //   it.target.style. transform = 'translateY(150px)'
+      // }
+    })
+  })
+  let reveal = document.querySelectorAll('.reveal')
+  observer.observe(reveal[0])
+  observer.observe(reveal[1])
+  observer.observe(reveal[2])
+  observer.observe(reveal[3])
+  observer.observe(reveal[4])
+  observer.observe(reveal[5])
+}
