10be5f0e78c0b088606d2bf8f31d82382c0ee1df
seoD22 <cobbt258@naver.com>
2023-03-31 13:58:49 +0900

    week2 자바스크립트 수정

--- week2-basic/js/week2.js
+++ week2-basic/js/week2.js
@@ -2,20 +2,12 @@ window.onload = ()=>{
   let observer = new IntersectionObserver((e)=>{
     e.forEach((el)=>{
       if(el.isIntersecting){
-        el.target.style.opacity = 1
-        el.target.style.transform = 'translateY(0)'
+        el.target.classList.add('active')
       }
-      // else{
-      //   it.target.style.opacity = 0
-      //   it.target.style. transform = 'translateY(150px)'
-      // }
     })
   })
   const reveal = document.querySelectorAll('.reveal')
-  observer.observe(reveal[0])
-  observer.observe(reveal[1])
-  observer.observe(reveal[2])
-  observer.observe(reveal[3])
-  observer.observe(reveal[4])
-  observer.observe(reveal[5])
+  for(let i = 0 ; i < 6; i++){
+    observer.observe(reveal[i])
+  }
 }
