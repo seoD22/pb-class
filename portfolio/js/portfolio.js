679a4113c808375281e068c3fa1764bf63273a9f
seoD22 <cobbt258@naver.com>
2023-04-10 14:28:33 +0900

    topBtn

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,6 +1,8 @@
 const $loader = document.querySelector('.loader');
 const $html = document.querySelector('html');
-const $fixed = document.querySelector('.fixed')
+const $body = document.querySelector('body');
+const $fixed = document.querySelector('.fixed');
+const $topBtn = document.querySelector('.topBtn');
 
 $html.style.overflow = 'hidden'; //로딩 중 스크롤 방지
 
@@ -13,11 +15,26 @@ window.addEventListener('load', ()=>{
 
 // scroll event
 window.addEventListener('scroll', ()=>{
-  const scroll = document.documentElement.scrollTop;
-  console.log(scroll)
-  if(scroll >= 100){
-    $fixed.classList.remove('none')
+  const scrollTop = document.documentElement.scrollTop;
+  const innerHeight = document.documentElement.clientHeight;
+  const bodyBottom = $body.scrollHeight;
+  // console.log(scroll)
+
+  // fixed
+  if(scrollTop + innerHeight >= bodyBottom){  // scroll 맨 아래 감지
+    $topBtn.classList.remove('none');
+    $fixed.classList.add('none');
+  }else if(scrollTop >= 100){
+    $fixed.classList.remove('none');
+    $topBtn.classList.add('none');
   }else{
-    $fixed.classList.add('none')
+    $fixed.classList.add('none');
+    $topBtn.classList.add('none');
   }
 })
+
+// topBtn click 이벤트
+$topBtn.addEventListener('click', ()=>{
+  window.scrollTo(0, 0)
+})
+
