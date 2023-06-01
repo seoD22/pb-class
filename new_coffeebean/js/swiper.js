0e3c0fd987549aa966858fd7229128641a9ab6f9
seoD22 <cobbt258@naver.com>
2023-06-01 15:15:25 +0900

    배너 추가

---
+++ new_coffeebean/js/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  // loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
