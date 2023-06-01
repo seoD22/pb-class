274cf50f32add5a31a21266c9eccae09b98a3711
seoD22 <cobbt258@naver.com>
2023-06-01 13:16:02 +0900

    onload 이벤트 수정

--- new_coffeebean/js/new.js
+++ new_coffeebean/js/new.js
@@ -2,9 +2,11 @@ const $body = document.querySelector('body')
 const $closeBtn = document.querySelector('.eventTab .close');
 
 // load event
-window.addEventListener('load', ()=>{
-  scrollTo(0, 0);
-})
+window.onload = ()=>{
+  setTimeout (()=>{
+  scrollTo(0,0);
+  },100);
+  }
 
 $closeBtn.addEventListener('click', ()=>{
   const adHeight = document.querySelector('.eventTab').clientHeight;
