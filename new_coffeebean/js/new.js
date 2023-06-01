0a2141008f838f550f5fcd3a357b4b92ea58bde2
seoD22 <cobbt258@naver.com>
2023-06-01 13:12:36 +0900

    header 완성

--- new_coffeebean/js/new.js
+++ new_coffeebean/js/new.js
@@ -1,6 +1,11 @@
 const $body = document.querySelector('body')
 const $closeBtn = document.querySelector('.eventTab .close');
 
+// load event
+window.addEventListener('load', ()=>{
+  scrollTo(0, 0);
+})
+
 $closeBtn.addEventListener('click', ()=>{
   const adHeight = document.querySelector('.eventTab').clientHeight;
   $body.style.transform =  `translateY(-${adHeight}px)`;
