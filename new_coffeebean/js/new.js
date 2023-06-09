4089a3f8714035a8fa5d698cebd0217a95115e23
seoD22 <cobbt258@naver.com>
2023-06-09 13:48:50 +0900

    relations section 완료

--- new_coffeebean/js/new.js
+++ new_coffeebean/js/new.js
@@ -1,5 +1,7 @@
 const $body = document.querySelector('body')
 const $closeBtn = document.querySelector('.eventTab .close');
+const $relBtn = document.querySelectorAll('.relations button');
+const $relList = document.querySelectorAll('.relations li')
 
 // load event
 window.onload = ()=>{
@@ -8,7 +10,15 @@ window.onload = ()=>{
   },100);
   }
 
+// event tab
 $closeBtn.addEventListener('click', ()=>{
   const adHeight = document.querySelector('.eventTab').clientHeight;
   $body.style.transform =  `translateY(-${adHeight}px)`;
 })
+
+// relations click event
+for(let i=0; i<$relBtn.length; i++){
+  $relBtn[i].addEventListener('click', ()=>{
+    $relList[i].classList.toggle('show');
+  })
+}
