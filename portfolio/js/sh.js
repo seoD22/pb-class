a1f72002fdaf79de5a989c405456443cbc80ea67
seohee_Yun <114120552+seoD22@users.noreply.github.com>
2024-03-26 21:12:07 +0900

    gsap_수정

--- portfolio/js/sh.js
+++ portfolio/js/sh.js
@@ -6,18 +6,27 @@ $(document).ready(function(){
       // sec_01
       const s01 = document.querySelector('section._01');
       let s01_dl = gsap.utils.toArray('section._01 dl');
-      let s01Tween = gsap.to(s01_dl, {
-        xPercent: -100 * (s01_dl.length -1),
-        ease: 'none',
-        scrollTrigger: {
-          trigger: s01,
-          pin: true,
-          scrub: 1,
-          start: 'center center',
-          end: '500%;',
-          // markers: true,
-        }
-      });
+      // let s01Tween = gsap.to(s01_dl, {
+      //   xPercent: -100 * (s01_dl.length -1),
+      //   ease: 'none',
+      //   scrollTrigger: {
+      //     trigger: s01,
+      //     pin: true,
+      //     scrub: 1,
+      //     start: 'center center',
+      //     end: '500%;',
+      //     // markers: true,
+      //   }
+      // });
+      
+      // ScrollTrigger.create({
+      //   trigger: s01,
+      //   start: 'top top',
+      //   scrub: 1,
+      //   pin: true,
+      //   pinSpacing: false,
+      // })
+
 
       // sec_01 스크롤 모션
       const s01_dt01 = document.querySelector('section._01 .dt_01');
@@ -27,7 +36,7 @@ $(document).ready(function(){
         scrollTrigger: {
           trigger: s01,
           start: 'center center',
-          end: '100%',
+          end: '200%',
           pin: true,
           scrub: 1,
           // markers: true
@@ -40,9 +49,13 @@ $(document).ready(function(){
       .set(s01_dt02, {xPercent: 200, opacity: 0})
       .set(s01_dt03, {xPercent: 200, opacity: 0})
 
-      .to(s01_dt01, {xPercent: 0, opacity: 1, duration: .5})
-      .to(s01_dt02, {xPercent: 0, opacity: 1, duration: .5})
-      .to(s01_dt03, {xPercent: 0, opacity: 1, duration: .5})
+      .to(s01_dt01, {xPercent: 0, opacity: 1, duration: 1})
+      .to(s01_dt02, {xPercent: 0, opacity: 1, duration: 1})
+      .to(s01_dt03, {xPercent: 0, opacity: 1, duration: 1})
+      .addLabel("label_01")
+      .to(s01_dt01, {opacity: 0, duration: 2}, 'label_01')
+      .to(s01_dt02, {opacity: 0, duration: 2}, 'label_01')
+      .to(s01_dt03, {opacity: 0, duration: 2}, 'label_01')
     }
   })
 
