207ee9c5b5e636e0f2653ef651d81d0a8c9468b5
seohee_Yun <114120552+seoD22@users.noreply.github.com>
2024-03-26 19:19:47 +0900

    sec01_gsap 1차

--- portfolio/js/sh.js
+++ portfolio/js/sh.js
@@ -1,5 +1,52 @@
 $(document).ready(function(){
 
+  ScrollTrigger.matchMedia({
+    '(min-width: 800px)': function(){
+
+      // sec_01
+      const s01 = document.querySelector('section._01');
+      let s01_dl = gsap.utils.toArray('section._01 dl');
+      let s01Tween = gsap.to(s01_dl, {
+        xPercent: -100 * (s01_dl.length -1),
+        ease: 'none',
+        scrollTrigger: {
+          trigger: s01,
+          pin: true,
+          scrub: 1,
+          start: 'center center',
+          end: '500%;',
+          // markers: true,
+        }
+      });
+
+      // sec_01 스크롤 모션
+      const s01_dt01 = document.querySelector('section._01 .dt_01');
+      const s01_dt02 = document.querySelector('section._01 .dt_02');
+      const s01_dt03 = document.querySelector('section._01 .dt_03');
+      let s01_timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: s01,
+          start: 'center center',
+          end: '100%',
+          pin: true,
+          scrub: 1,
+          // markers: true
+          invalidateOnRefresh: true,
+        },
+      });
+
+      s01_timeline
+      .set(s01_dt01, {xPercent: 200, opacity: 0})
+      .set(s01_dt02, {xPercent: 200, opacity: 0})
+      .set(s01_dt03, {xPercent: 200, opacity: 0})
+
+      .to(s01_dt01, {xPercent: 0, opacity: 1, duration: .5})
+      .to(s01_dt02, {xPercent: 0, opacity: 1, duration: .5})
+      .to(s01_dt03, {xPercent: 0, opacity: 1, duration: .5})
+    }
+  })
+
+
 
 
 });
