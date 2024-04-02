394daada407de4603419f84bc8589bf8f737afdf
seohee_Yun <cobbt258@naver.com>
2024-04-02 12:04:45 +0900

    포폴_다시 시작

---
+++ portfolio_02/js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // 기본 설정들
+    $(document).bind("contextmenu", function(e) {return false;});
+    $(document).bind('selectstart',function() {return false;}); 
+    AOS.init();
+    // 기본 설정들
+
+});
