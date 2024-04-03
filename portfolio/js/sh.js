39b4470255d0d68b79d44ad48ddbcc020cb5e335
seohee_Yun <cobbt258@naver.com>
2024-04-03 18:35:05 +0900

    pc_marquee 애니메이션 추가

--- portfolio/js/sh.js
+++ portfolio/js/sh.js
@@ -46,14 +46,91 @@ $(document).ready(function(){
       // sec_01
       const s01 = document.querySelector('section._01');
       const s01_dtSpan = document.querySelector('section._01 dt span');
-      const s01_imgBox = document.querySelector('section._02 img-wrap');
 
-      let s01_timeline = gsap.timeline({
+      ScrollTrigger.create({
+        trigger: s01,
+        pin: true,
+        // markers:true,
+        start: "top top",
+        end: "+=130%",
+      });
+      gsap.to(s01_dtSpan, {
         scrollTrigger: {
-          trigger: s01,
+          trigger: s01_dtSpan,
+          start: "top top",
+          end: "bottom+=800% center",
+          scrub: 1,
+          // markers:true,
+        },
+        x: 0, 
+      });
+
+      // sec_03
+      ScrollTrigger.create({
+        trigger: "section._03",
+        pin: true,
+        // markers:true,
+        start: "top top",
+        end: "+=500%",
+      });
+      // gsap.to("section._03 .sp_bg", {
+      //   scrollTrigger: {
+      //     trigger: "section._03",
+      //     start: "top center",
+      //     end: "bottom-=20% bottom+=50%",
+      //     scrub: 1,
+      //     // markers:true,
+      //   },
+      //   x: 0,
+      // });
+      // gsap.to("section._03 .img-border", {
+      //   scrollTrigger: {
+      //     trigger: "section._03",
+      //     start: "top center",
+      //     end: "bottom-=20% bottom",
+      //     scrub: 1,
+      //     // markers:true,
+      //   },
+      //   opacity: 1,
+      //   x: -40,
+      //   y: 40,
+      // });
+      
+      // gsap.to("section._03 .h3_box h3", {
+      //   scrollTrigger: {
+      //     trigger: "section._03",
+      //     start: "top center",
+      //     end: "bottom-=20% bottom",
+      //     scrub: 1,
+      //     // markers:true,
+      //   },
+      //   color: "#161616",
+      // });
+
+      // gsap.to("section._03 .marquee_group", {
+      //   scrollTrigger: {
+      //     trigger: "section._03",
+      //     start: "bottom-=70% center",
+      //     end: "bottom-=20% bottom",
+      //     scrub: 1,
+      //     markers:true,
+      //   },
+      //   opacity: 1,
+      // });
+
+      const s03_spBg = document.querySelector('section._03 .sp_bg');
+      const s03_border = document.querySelector('section._03 .img-border');
+      const s03_h3 = document.querySelector('section._03 .h3_box h3');
+      const s03_mq01 = document.querySelector('section._03 .marquee._01');
+      const s03_mq02 = document.querySelector('section._03 .marquee._02');
+      const s03_mq03 = document.querySelector('section._03 .marquee._03');
+
+      let s03_timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: 'section._03',
           start: 'center center',
-          end: '200%',
-          pin: true,
+          end: '500%',
+          // pin: true,
           scrub: 1,
           ease: 'none',
           // markers: true
@@ -61,36 +138,20 @@ $(document).ready(function(){
         },
       });
 
-      s01_timeline
-      .set(s01_dtSpan, {xPercent: -105})
-      // .set(s01_imgBox, {width: "20vw"})
+      s03_timeline
+      .set(s03_mq01, {xPercent: 100})
+      .set(s03_mq02, {xPercent: -200})
+      .set(s03_mq03, {xPercent: 100})
 
-      .to(s01_dtSpan, {xPercent: 0, duration: 3})
-      // .to(s01_imgBox, {width: "100vw"});
+      .addLabel("label_01")
+      .to(s03_spBg, {x: 0, duration: 1}, 'label_01')
+      .to(s03_border, {x: -40, y: 40, opacity: 1, duration: 1}, 'label_01')
+      .to(s03_h3, {color: '#161616'}, 'label_01')
+      .addLabel("label_02")
+      .to(s03_mq01, {opacity: 1, xPercent: -50, duration: 2,}, 'label_02')
+      .to(s03_mq02, {opacity: 1, xPercent: -50, duration: 2,}, 'label_02')
+      .to(s03_mq03, {opacity: 1, xPercent: -50, duration: 2,}, 'label_02')
 
-      // sec_03
-      gsap.to("section._03 .sp_bg", {
-        scrollTrigger: {
-          trigger: "section._03",
-          start: "top center",
-          end: "bottom-=20% bottom",
-          scrub: 1,
-          markers:true,
-        },
-        x: 0,
-      });
-      gsap.to("section._03 .h3_box h3", {
-        scrollTrigger: {
-          trigger: "section._03",
-          start: "top center",
-          end: "bottom-=20% bottom",
-          scrub: 1,
-          // markers:true,
-        },
-        color: "#161616",
-      });
-
-      // marquee
 
     } /// min-width: 801px
   }); 
